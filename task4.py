645a7151b86bf580d49737f85688bd71edc86482
Gavin Hua <gavin.hyl@outlook.com>
2024-02-24 11:05:50 -0800

    Inner iid.

--- task4.py
+++ task4.py
@@ -6,16 +6,17 @@ import matplotlib.pyplot as plt
 
 # Load the movies csv, which is used in multiple places below. This is read-only!
 MOVIES = pd.read_csv('movies.csv')
-print(len(MOVIES['Movie ID']))
+print(f"Number of unique movies: {len(MOVIES['Movie ID'])}")
 scores = {}
 for iid in MOVIES['Movie ID']:
-    scores.update({iid: })
+    scores.update({iid: 0})
 
 # Define a reader for Surprise
 reader = Reader()
 
 # Read the csv files
-train_df = pd.read_csv('data.csv')
+all_df = pd.read_csv('data.csv')
+train_df = pd.read_csv('train.csv')
 test_df = pd.read_csv('test.csv')
 train_df = train_df[['User ID', 'Movie ID', 'Rating']]
 print(train_df.describe())
@@ -26,12 +27,7 @@ train_set = Dataset.load_from_df(train_df, reader).build_full_trainset()
 algo = SVD(lr_all=0.03, reg_all=0.1)
 algo.fit(train_set)
 
-# Validate the model using mean error - sanity check
-# errors = []
-# print(test_df)
-# for _, (uid, iid, rating) in test_df.iterrows():
-#     errors.append(algo.predict(uid, iid).est - rating)
-# print(f"error: {sum(errors) / len(test_df.index)}")
+
 
 # Check factorization results
 U = algo.pu
@@ -39,13 +35,29 @@ print(f"U shape: {U.shape}")
 V = algo.qi
 print(f"V shape: {V.shape}")
 
+# zero_cnt = 0
+# for iid in MOVIES['Movie ID']:
+#     # for some reason unique has to be added here...
+#     if iid not in train_df['Movie ID'].unique():
+#         V = np.insert(V, iid, values=0, axis=0)
+#         zero_cnt += 1
+#         print(f"{iid} missing")
+# print(f"inserted zero rows: {zero_cnt}")
 V = V.T # maintain consistency with project description
 print(f"V shape (new): {V.shape}")
+
+# Validate the model using mean error - sanity check
+errors = []
+print(test_df)
+for _, (uid, iid, rating) in test_df.iterrows():
+    errors.append(algo.predict(uid, iid).est - rating)
+print(f"error: {sum(errors) / len(test_df.index)}")
 
 # perform SVD on the movie (V) matrix
 A, S, B = np.linalg.svd(V)
 print(A.shape)
 A_thin = (A[:, :2]).T
+# A_thin = A[:2]
 print(f"A thin shape: {A_thin.shape}")
 print(f"project shape: {(A_thin @ V).shape}")     # check that the dimensions is correct
 V_tilde = np.matmul(A_thin, V)        # project V onto the two most important dimensions
@@ -56,7 +68,8 @@ best_iid = [814, 1599, 1201, 1122, 1653, 1293, 1500, 1189, 1536, 1467]
 
 def produce_plot(iids: list, name: str):
     for iid in iids:
-        x, y = V_tilde[:, iid-1]
+        inner_iid = train_set.to_inner_iid(iid) # great. just great.
+        x, y = V_tilde[:, inner_iid]
         plt.annotate(f"{MOVIES['Movie Title'][iid-1]}", (x, y), fontsize=7)
         plt.scatter(x, y, c="tab:blue", s=6)
     plt.axvline(x=0, linestyle='--')
@@ -77,9 +90,9 @@ def first_movies_of_genre(genre: str, lim: int = 10):
     for _, row in MOVIES.iterrows():
         if row[genre] == 1:
             iids.append(row['Movie ID'])
-    counts = train_df['Movie ID'].value_counts()
+    counts = all_df['Movie ID'].value_counts()
     iids.sort(key=lambda id: counts.get(id), reverse=True)
-    return iids
+    return iids[:lim]
 
 # three genres: comedy, action, and documentary
 genres = ['Action', 'Comedy', 'Documentary']
